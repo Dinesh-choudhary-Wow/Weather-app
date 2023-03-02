a3a527448671155ebbac71674ddd98dc1b42ce4d
Mohammed-Saif0 <mohammedsaif9513@gmail.com>
2023-03-02 18:15:15 +0530

    m

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 import utils from './utils/utils.js';
 
-
-
 const searchButton = document.querySelector('#query-button');
 const searchInput = document.querySelector('#query-input');
 const weatherCard = document.querySelector('.weather-card');
 
 
-console.log(searchInput)
 utils.queryLocation(searchButton,searchInput,weatherCard);
--- utils/utils.js
+++ utils/utils.js
@@ -1,15 +1,15 @@
 
-const fetchWeather = async(inputElement) =>{
+const fetchWeather = async(inputElement,divElement) =>{
     const query = inputElement.value;
     inputElement.value = '';
     const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${query}%7D?unitGroup=us&key=HEG4F8F8KWRVJJMYWBWT267MR&contentType=json`;
     try{
         let response = await fetch(url);
         let data = await response.json();
-        return data;
+        divElement.innerText = "Address = "+data.resolvedAddress+" " +fahrenheitToCelsius(data.days[0].tempmax).toFixed(2) +" Degree Celcius";
     }
     catch(error){
-        return error
+        divElement.innerText =  `Location ${query} not found`;
     }
    
 }
@@ -19,12 +19,7 @@ const fahrenheitToCelsius = (temp) =>{
 //Function to get display (on divElement) weather whenever the buttonElement is clicked 
 const queryLocation =(buttonElement,inputElement,divElement) =>{
     buttonElement.addEventListener('click',function(){
-        fetchWeather(inputElement)
-        .then(data=>{
-            console.log(data);  
-            divElement.innerText = "Address = "+data.resolvedAddress+" " +fahrenheitToCelsius(data.days[0].tempmax).toFixed(2) +" Degree Celcius";
-        })
-        .catch(error=>console.log(error))
+        fetchWeather(inputElement,divElement)   
     })
 }
 
